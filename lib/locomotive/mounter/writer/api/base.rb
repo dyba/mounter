d3fda3655f0c48d5dcb9faef72ee68453942051a
Daniel Dyba <daniel.dyba@gmail.com>
2014-09-07 16:51:55 -0700

    Moved SiteWriter for API

--- lib/locomotive/mounter/writer/api/base.rb
+++ lib/locomotive/mounter/writer/api/base.rb
@@ -813,6 +813,326 @@ module Locomotive
             end
           end
         end 
+
+        class SiteWriter
+          attr_accessor :remote_site
+
+          # Check if the site has to be created before.
+          def prepare
+            self.output_title
+            self.fetch_site
+          end
+
+          # Create the site if it does not exist
+          def write
+            if self.site.persisted?
+              self.check_locales! unless self.force? # requirements
+
+              if self.force?
+                self.update_site
+              end
+            else
+              self.create_site
+            end
+          end
+
+          include Locomotive::Mounter::Utils::Output
+
+          attr_accessor :mounting_point, :runner
+
+          delegate :default_locale, :locales, :site, :sprockets, to: :mounting_point
+
+          delegate :content_assets_writer, to: :runner
+
+          delegate :force?, to: :runner
+
+          def initialize(mounting_point, runner)
+            self.mounting_point = mounting_point
+            self.runner         = runner
+          end
+
+          # By setting the data option to true, user content (content entries and
+          # editable elements from page) can be pushed too.
+          # By default, its value is false.
+          #
+          # @return [ Boolean ] True if the data option has been set to true
+          #
+          def data?
+            self.runner.parameters[:data] || false
+          end
+
+          # Get remote resource(s) by the API
+          #
+          # @param [ String ] resource_name The path to the resource (usually, the resource name)
+          # @param [ String ] locale The locale for the request
+          # @param [ Boolean ] raw True if the result has to be converted into object.
+          #
+          # @return [ Object] The object or a collection of objects.
+          #
+          def get(resource_name, locale = nil, raw = false)
+            params = { query: {} }
+
+            params[:query][:locale] = locale if locale
+
+            response  = Locomotive::Mounter::EngineApi.get("/#{resource_name}.json", params)
+            data      = response.parsed_response
+
+            if response.success?
+              return data if raw
+              self.raw_data_to_object(data)
+            else
+              raise WriterException.new(data['error'])
+            end
+          end
+
+          # Create a resource by the API.
+          #
+          # @param [ String ] resource_name The path to the resource (usually, the resource name)
+          # @param [ Hash ] params The attributes of the resource
+          # @param [ String ] locale The locale for the request
+          # @param [ Boolean ] raw True if the result has to be converted into object.
+          #
+          # @return [ Object] The response of the API or nil if an error occurs
+          #
+          def post(resource_name, params, locale = nil, raw = false)
+            params_name = resource_name.to_s.split('/').last.singularize
+
+            query = { query: { params_name => params } }
+
+            query[:query][:locale] = locale if locale
+
+            response  = Locomotive::Mounter::EngineApi.post("/#{resource_name}.json", query)
+            data      = response.parsed_response
+
+            if response.success?
+              return data if raw
+              self.raw_data_to_object(data)
+            else
+              message = data
+
+              message = data.map do |attribute, errors|
+                "      #{attribute} => #{[*errors].join(', ')}\n".colorize(color: :red)
+              end.join("\n") if data.respond_to?(:keys)
+
+              raise WriterException.new(message)
+
+              # self.log "\n"
+              # data.each do |attribute, errors|
+              #   self.log "      #{attribute} => #{[*errors].join(', ')}\n".colorize(color: :red)
+              # end if data.respond_to?(:keys)
+              # nil # DEBUG
+            end
+          end
+
+          # Update a resource by the API.
+          #
+          # @param [ String ] resource_name The path to the resource (usually, the resource name)
+          # @param [ String ] id The unique identifier of the resource
+          # @param [ Hash ] params The attributes of the resource
+          # @param [ String ] locale The locale for the request
+          #
+          # @return [ Object] The response of the API or nil if an error occurs
+          #
+          def put(resource_name, id, params, locale = nil)
+            params_name = resource_name.to_s.split('/').last.singularize
+
+            query = { query: { params_name => params } }
+
+            query[:query][:locale] = locale if locale
+
+            response  = Locomotive::Mounter::EngineApi.put("/#{resource_name}/#{id}.json", query)
+            data      = response.parsed_response
+
+            if response.success?
+              self.raw_data_to_object(data)
+            else
+              message = data
+
+              message = data.map do |attribute, errors|
+                "      #{attribute} => #{[*errors].join(', ')}" #.colorize(color: :red)
+              end.join("\n") if data.respond_to?(:keys)
+
+              raise WriterException.new(message)
+
+              # data.each do |attribute, errors|
+              #   self.log "\t\t #{attribute} => #{[*errors].join(', ')}".colorize(color: :red)
+              # end if data.respond_to?(:keys)
+              # nil # DEBUG
+            end
+          end
+
+          def safe_attributes
+            %w(_id)
+          end
+
+          # Loop on each locale of the mounting point and
+          # change the current locale at the same time.
+          def each_locale(&block)
+            self.mounting_point.locales.each do |locale|
+              Locomotive::Mounter.with_locale(locale) do
+                block.call(locale)
+              end
+            end
+          end
+
+          # Return the absolute path from a relative path
+          # pointing to an asset within the public folder
+          #
+          # @param [ String ] path The path to the file within the public folder
+          #
+          # @return [ String ] The absolute path
+          #
+          def absolute_path(path)
+            File.join(self.mounting_point.path, 'public', path)
+          end
+
+          # Take a path and convert it to a File object if possible
+          #
+          # @param [ String ] path The path to the file within the public folder
+          #
+          # @return [ Object ] The file
+          #
+          def path_to_file(path)
+            File.new(self.absolute_path(path))
+          end
+
+          # Take in the source the assets whose url begins by "/samples",
+          # upload them to the engine and replace them by their remote url.
+          #
+          # @param [ String ] source The source text
+          #
+          # @return [ String ] The source with remote urls
+          #
+          def replace_content_assets!(source)
+            return source if source.blank?
+
+            source.to_s.gsub(/\/samples\/\S*\.[a-zA-Z0-9]+/) do |match|
+              url = self.content_assets_writer.write(match)
+              url || match
+            end
+          end
+
+          protected
+
+          def response_to_status(response)
+            response ? :success : :error
+          end
+
+          # Convert raw data into the corresponding object (Page, Site, ...etc)
+          #
+          # @param [ Hash ] data The attributes of the object
+          #
+          # @return [ Object ] A new instance of the object
+          #
+          def raw_data_to_object(data)
+            case data
+            when Hash then data.to_hash.delete_if { |k, _| !self.safe_attributes.include?(k) }
+            when Array
+              data.map do |row|
+                # puts "#{row.inspect}\n---" # DEBUG
+                row.delete_if { |k, _| !self.safe_attributes.include?(k) }
+              end
+            else
+              data
+            end
+          end
+
+          #=============================================================================
+          # API CLIENT RESPONSIBILITY
+          #=============================================================================
+          #
+          # Create the current site in all the locales
+          #
+          def create_site
+            # create it in the default locale
+            Mounter.with_locale(self.default_locale) do
+              self.output_locale
+
+              self.output_resource_op self.site
+
+              if (site = self.post(:sites, self.site.to_hash(false), Mounter.locale)).nil?
+                raise Mounter::WriterException.new('Sorry, we are unable to create the site.')
+              else
+                self.site._id = site['id']
+                self.output_resource_op_status self.site
+              end
+            end
+
+            # update it in other locales
+            self.update_site(true)
+          end
+
+          #=============================================================================
+          # API CLIENT RESPONSIBILITY
+          #=============================================================================
+          #
+          # Update the current site in all the locales
+          #
+          # @param [ Boolean ] exclude_current_locale Update the site for all the locales other than the default one.
+          #
+          def update_site(exclude_current_locale = false)
+            self.each_locale do |locale|
+              next if exclude_current_locale && locale.to_s == self.default_locale.to_s
+
+              self.output_locale
+
+              begin
+                self.output_resource_op self.site
+
+                self.put(:sites, self.site._id, self.site.to_hash(false), locale)
+
+                self.output_resource_op_status self.site
+              rescue Exception => e
+                self.output_resource_op_status self.site, :error, e.message
+              end
+            end
+          end
+
+          def safe_attributes
+            %w(id name locales timezone)
+          end
+
+          def fetch_site
+            begin
+              self.get(:current_site).tap do |_site|
+                if _site
+                  self.remote_site  = _site
+                  self.site._id     = _site['id']
+                end
+              end
+            rescue WriterException => e
+              nil
+            end
+          end
+
+          # To push all the other resources, the big requirement is to
+          # have the same locales between the local site and the remote one.
+          #
+          def check_locales!
+            default_locale  = self.mounting_point.default_locale.to_s
+            locales         = self.site.locales.map(&:to_s)
+            remote_locales  = self.remote_site['locales']
+            message         = nil
+
+            unless locales.all? { |l| remote_locales.include?(l) }
+              message = "Your site locales (#{locales.join(', ')}) do not match exactly the ones of your target (#{remote_locales.join(', ')})"
+            end
+
+            if default_locale != remote_locales.first
+              message = "Your default site locale (#{default_locale.inspect}) is not the same as the one of your target (#{remote_locales.first.inspect})"
+            end
+
+            if message
+              self.output_resource_op self.site
+              self.output_resource_op_status self.site, :error, message
+              raise Mounter::WriterException.new('Use the force option in order to force your locale settings.')
+            end
+          end
+
+          def has_admin_rights?
+            self.get(:my_account, nil, true)['admin']
+          end
+        end
       end # Api
     end # Writer
   end # Mounter
